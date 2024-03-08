55776e9549e3596cad390cfebd30de9ed9e777cf
Jeremy Schuler <jeremy.schuler@vjss.ch>
2024-03-08 14:23:36 +0100

    modified:   dashboard/apex_teams.html 	new file:   dashboard/js/apex_teams.js 	modified:   dashboard/js/panel.js 	modified:   dashboard/panel.html 	modified:   extension/index.js

--- extension/index.js
+++ extension/index.js
@@ -1,2 +1,24 @@
 module.exports = function (nodecg) {
+    nodecg.Replicant('smash_players', {defaultValue: {
+        Player0:{
+            Tag: "TAG1",
+            Name: "Peiass1"
+        },
+        Player1:{
+            Tag: "TAG2",
+            Name: "Peiass2"
+        },
+        Player2:{
+            Tag: "TAG3",
+            Name: "Peiass3"
+        },
+        Player3:{
+            Tag: "TAG4",
+            Name: "Peiass4"
+        }
+        }
+    });
+    nodecg.Replicant('apex_teams', {defaultValue: 
+        ["Team1", "Team2", "Team3", "Team4", "Team5", "Team6", "Team7", "Team8", "Team9", "Team10", "Team11", "Team12", "Team13", "Team14", "Team15", "Team16", "Team17", "Team18", "Team19", "Team20",]
+})
 };
